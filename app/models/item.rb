0b558e6c8a1d0fef66256773770d7924528fe0b2
Deepak kumar <55788633+Deepak18-06@users.noreply.github.com>
2024-01-16 15:21:58 +0530

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,4 +11,29 @@ class Item < ApplicationRecord
         d = 2
         result = c + d
     end
+    def calculate3
+        c = 1
+        d = 2
+        result = c + d
+    end
+    def calculate4
+        c = 1
+        d = 2
+        result = c + d
+    end
+    def calculate5
+        c = 1
+        d = 2
+        result = c + d
+    end
+    def calculate6
+        c = 1
+        d = 2
+        result = c + d
+    end
+    def calculate7
+        c = 1
+        d = 2
+        result = c + d
+    end
 end
